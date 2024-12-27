f60ea2a9e6f93ab4c1a75fd3de65a13275ab9a03
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2024-12-27 23:29:00 +0530

    useContext is done

--- frontend/src/screens/Register.jsx
+++ frontend/src/screens/Register.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { UserContext } from '../context/user.context.jsx';
 import axios from '../config/axios.js';
 
 const Register = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const { setUser } = useContext(UserContext);
+
     const navigate = useNavigate();
 
     const submitHandler = (e) => {
@@ -14,6 +18,8 @@ const Register = () => {
             password
         }).then((res) => {
             console.log(res.data);
+            localStorage.setItem('token', res.data.token);
+            setUser(res.data.user);
             navigate('/');
         }).catch((err) => {
             console.log(err.response.data);
