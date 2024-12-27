50686c68f3a73917649696cdcba4e3f36518521a
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2024-12-27 18:51:10 +0530

    login, logout done

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import redisClient from "../services/redis.service.js";
 
 
 export const authUser = async (req, res, next) => {
@@ -6,6 +7,14 @@ export const authUser = async (req, res, next) => {
         const token = req.cookies.token || req.headers.authorization.split(' ')[ 1 ];
 
         if (!token) {
+            return res.status(401).send({ error: 'Unauthorized User' });
+        }
+
+        const isBlacklisted = await redisClient.get(token);
+
+        if (isBlacklisted) {
+            res.cookie('token', '');
+
             return res.status(401).send({ error: 'Unauthorized User' });
         }
 
